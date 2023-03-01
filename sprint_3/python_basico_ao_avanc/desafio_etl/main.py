bd33c911f0c2671d40139acbefc96ed351db441c
Natalia Guimaraes <guimaraessnatalia@gmail.com>
2023-03-01 08:34:39 -0300

    update: files

--- sprint_3/python_basico_ao_avanc/desafio_etl/main.py
+++ sprint_3/python_basico_ao_avanc/desafio_etl/main.py
@@ -7,13 +7,32 @@ leia o arquivo actors.csv utilizando Python como linguagem de programação
 e depois de obter as repostas necessárias armazene cada um dos resultados em um dos arquivos “txt” criados.
 
 Perguntas dessa tarefa
-O ator/atriz com maior número de filmes e o respectivo número de filmes.
+1. O ator/atriz com maior número de filmes e o respectivo número de filmes.
 
-A média do número de filmes por autor.
+2. A média do número de filmes por autor.
 
-O ator/atriz com a maior média por filme.
+3. O ator/atriz com a maior média por filme.
 
-O nome do(s) filme(s) mais frequente(s) e sua respectiva frequência.
+4. O nome do(s) filme(s) mais frequente(s) e sua respectiva frequência.
 
-A lista dos Autores ordenada por pagamento. Do mais bem pago para o menos bem pago
+5. A lista dos Autores ordenada por pagamento. Do mais bem pago para o menos bem pago
 """
+
+
+from csv import reader
+
+with open ('actors.csv') as arquivo:
+  leitura = reader(arquivo)
+  for linha in leitura:
+  #cada linha é uma lista
+    print(linha)
+
+  linha = []
+
+  valores = linha.strip().split(',')
+
+  for i in range(len(valores)):
+    valores[i] = valores[i].replace(',', ' ')
+
+linha_modificada = ','.join(valores)
+linha.append(linha_modificada)
