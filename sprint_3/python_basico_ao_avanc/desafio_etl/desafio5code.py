e1d709d50229c4da4e3f68d3219ba785714e611a
Natalia Guimaraes <guimaraessnatalia@gmail.com>
2023-03-02 10:51:06 -0300

    att: files

--- sprint_3/python_basico_ao_avanc/desafio_etl/desafio5code.py
+++ sprint_3/python_basico_ao_avanc/desafio_etl/desafio5code.py
@@ -1,19 +1,16 @@
+#5 A lista dos Autores ordenada por pagamento. Do mais bem pago para o menos bem pago: + bem pago: total gross
+  
 from csv import DictReader
 
 with open('./actors.csv') as file:
     csv_reader = DictReader(file)
     data = list(csv_reader)
-    
-    maiorTotalGross = 0
-    nomeAtor = ''
-    for elementos in data:
-        TotalGross =  float(elementos['Total Gross'])
-    if(maiorTotalGross < TotalGross):
-        nomeAtor = elementos['Actor']
-        maiorTotalGross = TotalGross
-        
-        
-    #sorted(data, key=lambda x: x['Total Gross'], reverse=True)
-    print(data)
 
-#nao ta funcionando lamba e print... arrumar amanhã.
+    sorted_data = sorted(data, key=lambda x: float(x['Total Gross']), reverse=True)
+  
+    print(f'A lista de atores em ordem de pagamento do mais bem pago para o menos bem pago é:\n')
+    
+    for elementos in sorted_data:
+      TotalGross = float(elementos['Total Gross'])
+      nomeAtor = elementos['Actor']
+      print(nomeAtor)
