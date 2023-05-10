fd4b8259c83c7a81ce8b1ae2633dacbcad799c44
Natalia Guimaraes <104440384+nataliasguimaraes@users.noreply.github.com>
2023-05-10 03:49:44 -0300

    add: prints desafio 2

--- sprint_8/desafio_parte2/lambda_function.py
+++ sprint_8/desafio_parte2/lambda_function.py
@@ -24,22 +24,27 @@ def lambda_handler(event, context):
     api_key = '***'
     url_base = 'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=pt-BR"'
 
-    movies = []
-    for movie_id in filtered_ids:
-        url = url_base.format(movie_id=movie_id, api_key=api_key)
-        response = requests.get(url)
-        if response.status_code == 200:
-            movie_data = json.loads(response.content)
-            movies.append(movie_data)
 
-    data = {"movies": movies}
+    batch_size = 1000
+    num_batches = (len(filtered_ids) + batch_size - 1) // batch_size
 
-    json_data = json.dumps(data, ensure_ascii=False, indent=4)
+    for i in range(num_batches):
+        batch_ids = filtered_ids[i*batch_size : (i+1)*batch_size]
+        movies = []
+        for movie_id in batch_ids:
+            url = url_base.format(movie_id=movie_id, api_key=api_key)
+            response = requests.get(url)
+            if response.status_code == 200:
+                movie_data = json.loads(response.content)
+                movies.append(movie_data)
 
-    file_name = 'tmdb_filtered_movies.json'
+        data = {"movies": movies}
+
+        json_data = json.dumps(data, ensure_ascii=False, indent=4)
+
+        file_name = f"tmdb_fmovies_{i+1}.json"
 
 # WRITE JSON DATA INTO AWS BUCKET
-    s3 = boto3.resource('s3')
-    bucket_name = 'natalias-s3-bucket'
-    bucket_movies_directory = f'Raw/Local/JSON/Movies/{datetime.now().strftime("%Y/%m/%d")}/{file_name}'
-    s3.Object(bucket_name, bucket_movies_directory).put(Body=json_data)
+
+        bucket_movies_directory = f'Raw/Local/JSON/Movies/{datetime.now().strftime("%Y/%m/%d")}/{file_name}'
+        s3.Object(bucket_name, bucket_movies_directory).put(Body=json_data)
